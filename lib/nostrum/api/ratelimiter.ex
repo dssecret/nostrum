169a98f279715651259933531970d56ed77ba48e
tiksan <webmaster@deek.sh>
2026-03-15 22:50:30 -0500

    test
    Signed-off-by: tiksan <webmaster@deek.sh>

--- lib/nostrum/api/ratelimiter.ex
+++ lib/nostrum/api/ratelimiter.ex
@@ -1033,9 +1033,7 @@ defmodule Nostrum.Api.Ratelimiter do
         {:next_event, :internal, {:requeue, {request, client}, :abnormal_close}}
       end)
 
-    # new_data = %{empty_state(config) | outstanding: outstanding}
-    outstanding |> inspect() |> Logger.info()
-    new_data = empty_state(config)
+    new_data = %{empty_state(config) | outstanding: outstanding}
     {:next_state, :disconnected, new_data, replies}
   end
 
